8715017b5eaae74a1ec06c9a723ae6e0ab1d6995
dogebpaz <134856528+dogebpaz@users.noreply.github.com>
2023-07-20 17:26:22 +0200

    Update msgboxsimpletest.cs

--- msgboxsimpletest.cs
+++ msgboxsimpletest.cs
@@ -1,4 +1,10 @@
-public void MsgBox()
+using System;
+using System.Windows.Forms;
+
+namespace MsgBox
+{
+static public void MsgBox()
 {
 MessageBox.Show("its a simple test please do not ask about that");
+}
 }
